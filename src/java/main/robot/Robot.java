7e9264dcb6a1bcbaa151e7f7f09cefcc97c71997
Simar Kareer <skareer@outlook.com>
2017-10-03 16:12:59 -0400

    Trying to deploy properly(deleted stuff ill revert later)

--- src/java/main/robot/Robot.java
+++ src/java/main/robot/Robot.java
@@ -19,42 +19,46 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
-
+/*
 	private TankDrive drive;
 	private ContinuousRange leftSpeed;
 	private ContinuousRange rightSpeed;
 	private SendableChooser autoChooser;
 	private static final Map<String,Supplier<Command>> AUTONOMOUS_SELECTION = new HashMap<>();
-	
+
 	
 	public static final class Auto{
-		public static final String MOTION_PROF_1 = "Motion Profile 1";
+		//public static final String MOTION_PROF_1 = "Motion Profile 1";
 	}
 	
     @Override
     public void robotInit() {
+	    /*
     	Motor leftMain = Hardware.Motors.talon(RobotMap.PORT_MOTOR_DRIVE_LEFT_MAIN);
     	Motor left2 = Hardware.Motors.talon(RobotMap.PORT_MOTOR_DRIVE_LEFT_2);
-    	Motor left3 = Hardware.Motors.talon(RobotMap.PORT_MOTOR_DRIVE_LEFT_3);
-    	Motor left = Motor.compose(leftMain, left2, left3);
-    	
+    	//Motor left3 = Hardware.Motors.talon(RobotMap.PORT_MOTOR_DRIVE_LEFT_3);
+    	//Motor left = Motor.compose(leftMain, left2, left3);
+		Motor left = Motor.compose(leftMain, left2);
+
     	Motor rightMain = Hardware.Motors.talon(RobotMap.PORT_MOTOR_DRIVE_RIGHT_MAIN);
     	Motor right2 = Hardware.Motors.talon(RobotMap.PORT_MOTOR_DRIVE_RIGHT_2);
-    	Motor right3 = Hardware.Motors.talon(RobotMap.PORT_MOTOR_DRIVE_RIGHT_3);
-    	Motor right = Motor.compose(leftMain, right2, right3).invert();
-    	
+    	//Motor right3 = Hardware.Motors.talon(RobotMap.PORT_MOTOR_DRIVE_RIGHT_3);
+    	//Motor right = Motor.compose(rightMain, right2, right3).invert();
+		Motor right = Motor.compose(rightMain, right2);
+
     	Gamepad xboxDrive = Hardware.HumanInterfaceDevices.logitechDualAction(RobotMap.PORT_XBOX_DRIVE);
     	leftSpeed = xboxDrive.getLeftY();
     	rightSpeed = xboxDrive.getRightY();
     	
     	drive = new TankDrive(left, right);
     	
-    	
+
     	autoChooser = new SendableChooser();
     	autoChooser.addDefault(Auto.MOTION_PROF_1, Auto.MOTION_PROF_1);
     	SmartDashboard.putData("Autonomous Modes", autoChooser);
     	AUTONOMOUS_SELECTION.clear();
     	AUTONOMOUS_SELECTION.put(Auto.MOTION_PROF_1, ()->new MotionProf1(drive));
+    	*/
     }
 
     @Override
