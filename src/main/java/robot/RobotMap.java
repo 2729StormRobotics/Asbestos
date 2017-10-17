a08995f2d2578dc9122d6d54da21a3f2ec810404
Simar Kareer <skareer@outlook.com>
2017-10-17 17:28:06 -0400

    Fixed ports numbers

--- src/main/java/robot/RobotMap.java
+++ src/main/java/robot/RobotMap.java
@@ -3,12 +3,12 @@ package robot;
 public class RobotMap {
 	//Motors
 	public static final int
-	PORT_MOTOR_DRIVE_LEFT_MAIN 			= 13,
-	PORT_MOTOR_DRIVE_LEFT_2 			= 14,
-	PORT_MOTOR_DRIVE_LEFT_3 			= 15,
-	PORT_MOTOR_DRIVE_RIGHT_MAIN 		= 10,
-	PORT_MOTOR_DRIVE_RIGHT_2 			= 11,
-	PORT_MOTOR_DRIVE_RIGHT_3 			= 12;
+	PORT_MOTOR_DRIVE_LEFT_MAIN 			= 1, //Front Left
+	PORT_MOTOR_DRIVE_LEFT_2 			= 2, //Back left
+	PORT_MOTOR_DRIVE_LEFT_3 			= 3,
+	PORT_MOTOR_DRIVE_RIGHT_MAIN 		= 11,
+	PORT_MOTOR_DRIVE_RIGHT_2 			= 12,
+	PORT_MOTOR_DRIVE_RIGHT_3 			= 13;
 	
 	//Controllers
 	public static final int
