1c04d5fe5ca2c6aec24f32ef49f19419316f5916
Simar Kareer <skareer@outlook.com>
2017-10-17 16:34:02 -0400

    robot code uploads, problem with iterative robot
    The robot doesnt move yet

--- src/main/java/robot/RobotMap.java
+++ src/main/java/robot/RobotMap.java
@@ -3,9 +3,9 @@ package robot;
 public class RobotMap {
 	//Motors
 	public static final int
-	PORT_MOTOR_DRIVE_LEFT_MAIN 			= 20,
-	PORT_MOTOR_DRIVE_LEFT_2 			= 21,
-	PORT_MOTOR_DRIVE_LEFT_3 			= 22,
+	PORT_MOTOR_DRIVE_LEFT_MAIN 			= 13,
+	PORT_MOTOR_DRIVE_LEFT_2 			= 14,
+	PORT_MOTOR_DRIVE_LEFT_3 			= 15,
 	PORT_MOTOR_DRIVE_RIGHT_MAIN 		= 10,
 	PORT_MOTOR_DRIVE_RIGHT_2 			= 11,
 	PORT_MOTOR_DRIVE_RIGHT_3 			= 12;
