968f7636cc027156d9f482125c84911f4c2ad717
Mr. Bross <32577977+zbross@users.noreply.github.com>
2017-11-30 19:42:42 -0500

    Added Motors

--- src/main/java/robot/Robot.java
+++ src/main/java/robot/Robot.java
@@ -1,10 +1,29 @@
 package robot;
 
+import com.ctre.CANTalon;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 public class Robot extends IterativeRobot {
+
+    private static final CANTalon _leftMain = new CANTalon(RobotMap.PORT_MOTOR_DRIVE_LEFT_MAIN);
+    private static final CANTalon _left2 = new CANTalon(RobotMap.PORT_MOTOR_DRIVE_LEFT_2);
+
+    private static final CANTalon _rightMain = new CANTalon(RobotMap.PORT_MOTOR_DRIVE_RIGHT_MAIN);
+    private static final CANTalon _right2 = new CANTalon(RobotMap.PORT_MOTOR_DRIVE_RIGHT_2);
+
     @Override
-    public void robotInit() { }
+    public void robotInit() {
+
+        _left2.changeControlMode(CANTalon.TalonControlMode.Follower);
+        _left2.set(_leftMain.getDeviceID());
+
+        _right2.changeControlMode(CANTalon.TalonControlMode.Follower);
+        _right2.set(_rightMain.getDeviceID());
+
+
+        _rightMain.setInverted(true);
+
+    }
 
     @Override
     public void disabledInit() { }
